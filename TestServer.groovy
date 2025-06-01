c7709148bce1c6283228c3eb488a0ce2c5f30125
Kevin Harrington <mad.hephaestus@gmail.com>
2025-06-01 14:20:17 -0400

    Use a sensable number

--- TestServer.groovy
+++ TestServer.groovy
@@ -4,4 +4,4 @@ int port = 3742;
 File f = new File("/opt/File.txt");
 CSGClient.start(hostname, port, f);
 // Set a low number to ensure the Server is used. this defaults to 200
-CSG.setMinPolygonsForOffloading(4);
+CSG.setMinPolygonsForOffloading(500);
