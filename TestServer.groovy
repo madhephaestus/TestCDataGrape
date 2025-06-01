24c87e0ab2aa8d35ba491c639fde2b11050d7db7
Kevin Harrington <mad.hephaestus@gmail.com>
2025-06-01 14:17:21 -0400

    Add hostname

--- TestServer.groovy
+++ TestServer.groovy
@@ -1,5 +1,6 @@
 // code here
-
+String hostname = "localhost";
+int port = 3742;
 File f = new File("/opt/File.txt");
 CSGClient.start(hostname, port, f);
 // Set a low number to ensure the Server is used. this defaults to 200
